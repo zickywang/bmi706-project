259ee27b690eee3c1a41f8010a086c60819535e2
linzi-yu <linzi_yu@hms.harvard.edu>
2022-04-26 23:10:42 -0400

    load data

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,26 +12,22 @@ import streamlit as st
 def load_data():
     ## {{ CODE HERE }} ##
     # Move this code into `load_data` function
-    cancer_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/cancer_ICD10.csv").melt(  # type: ignore
-        id_vars=["Country", "Year", "Cancer", "Sex"],
-        var_name="Age",
-        value_name="Deaths",
-    )
+    covid_df = pd.read_csv(
+        "https://covid.ourworldindata.org/data/owid-covid-data.csv")
+    # pop_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/population.csv").melt(  # type: ignore
+    #     id_vars=["Country", "Year", "Sex"],
+    #     var_name="Age",
+    #     value_name="Pop",
+    # )
 
-    pop_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/population.csv").melt(  # type: ignore
-        id_vars=["Country", "Year", "Sex"],
-        var_name="Age",
-        value_name="Pop",
-    )
+    # df = pd.merge(left=cancer_df, right=pop_df, how="left")
+    # df["Pop"] = df.groupby(["Country", "Sex", "Age"])[
+    #     "Pop"].fillna(method="bfill")
+    # df.dropna(inplace=True)
 
-    df = pd.merge(left=cancer_df, right=pop_df, how="left")
-    df["Pop"] = df.groupby(["Country", "Sex", "Age"])[
-        "Pop"].fillna(method="bfill")
-    df.dropna(inplace=True)
-
-    df = df.groupby(["Country", "Year", "Cancer",
-                    "Age", "Sex"]).sum().reset_index()
-    df["Rate"] = df["Deaths"] / df["Pop"] * 100_000
+    # df = df.groupby(["Country", "Year", "Cancer",
+    #                 "Age", "Sex"]).sum().reset_index()
+    # df["Rate"] = df["Deaths"] / df["Pop"] * 100_000
 
     return df
 
