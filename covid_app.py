d31232d5da2f6e360523ff3a5ea1335ea3c3da72
linzi-yu <linzi_yu@hms.harvard.edu>
2022-04-28 13:12:25 -0400

    clear

--- covid_app.py
+++ covid_app.py
@@ -24,33 +24,3 @@ st.write("### Source: Our World in Data")
 ### Plot 2 ###
 
 ### Plot 3: bar chart ###
-
-selector = alt.selection_single(
-    fields=['Age']
-)
-
-chart_base = alt.Chart(subset
-                       ).properties(
-    width=600,
-    height=300
-).add_selection(
-    selector
-)
-
-
-chart_pop = chart_base.mark_bar().encode(
-    y=alt.Y("Country"),
-    x=alt.X("Pop", title="Population"),
-    tooltip=["Pop"],
-).properties(
-    title=f"Population of {'males' if sex == 'M' else 'females'} in the selected countries in {year} for the selected age group",
-).transform_filter(
-    selector
-)
-
-chart_link = alt.vconcat(chart_rate, chart_pop
-                         ).resolve_scale(
-    color='independent'
-)
-
-st.altair_chart(chart_link, use_container_width=True)
