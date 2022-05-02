0e042bd5cf006c2e073cf7fedb8f7e792e581210
zickywang <66640582+zickywang@users.noreply.github.com>
2022-05-02 04:17:55 -0400

    merge G2 and G3

--- covid_app.py
+++ covid_app.py
@@ -21,14 +21,19 @@ def load_data():
     return covid_df
 
 
-# Uncomment the next line when finished
+# Load data and normalize data for G3
 df = load_data()
 df_norm = df.iloc[:, 4:]/df.iloc[:, 4:].max()
 df_norm.columns = [
     str(df_norm) + '_norm' for df_norm in df_norm.columns]
 df_norm = pd.concat((df.iloc[:, :4], df_norm), 1)
 
-### Radio selector for selection of COVID statistics ###
+st.write("# COVID Data Visualizer")
+
+
+### Radio selector for selection of COVID statistics: G1, G2, G3 ###
+st.write("##### Select a COVID statistics for all visualizations below")
+
 cov_stats = [
     'New Cases',
     'Hospitalizations',
@@ -39,24 +44,119 @@ cov_stats = [
 selected_stat = st.radio("Covid Statistics", options=cov_stats)
 #############
 
-### Title of G3 ###
-st.write("# National Conditions and Covid Statistics across Different Countries")
+
+### Dropdown for Countries: G2 and G3 ###
+default_countries = [
+    'Japan',
+    'United Kingdom',
+    'Italy',
+    'France',
+]
+option_countries = df["location"].unique()
+countries = st.multiselect(
+    "Select Countries", options=option_countries, default=default_countries)
+##############
+
+
+############### G2 ###############
+### Slider for selection of date range: G2 ###
+start_date, end_date = st.slider("Select a range of date", min_value=min(df['date']), max_value=max(df['date']), value=[datetime.date(2021, 4, 1), datetime.date(2022, 1, 1)], format="YYYY-MMM-DD")
+start_date = str(start_date)
+end_date = str(end_date)
+df_g2 = df.loc[(df['date'] > start_date) & (df['date'] < end_date)]
 #############
 
-# ### Filter for COVID Statistics ###
-# # total deaths per million
-# stats_options = [
-#     'new_cases_per_million',
-#     'total_deaths_per_million',
-#     'hosp_patients_per_million',
-#     'icu_patients_per_million'
-# ]
-# covid_stats = st.selectbox(
-#     "Covid statistics", options=stats_options)
-# #############
+# filter countries
+df_g2 = df_g2[df_g2["location"].isin(countries)]
+
+# Plot2 Headline 
+st.write("#### Daily {} per Million People across Countries".format(selected_stat))
+
+# multi-line tooltip
+nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                        fields=['date'], empty='none')
+
+selectors = alt.Chart(df_g2).mark_point().encode(
+    x='date',
+    opacity=alt.value(0),
+).add_selection(
+    nearest
+)
+
+# base line plot
+if selected_stat == 'New Cases':
+    base = alt.Chart(df_g2
+    ).mark_line().encode(
+        x='date',
+        y='new_cases_smoothed_per_million',
+        color='location',
+        tooltip = ['new_cases_smoothed_per_million', 'location']
+    )
+    text = base.mark_text(align='left', dx=5, dy=-5).encode(
+    text=alt.condition(nearest, 'new_cases_smoothed_per_million', alt.value(' '))
+    )
+elif selected_stat == 'Hospitalizations':
+    base = alt.Chart(df_g2
+    ).mark_line().encode(
+        x='date',
+        y='hosp_patients_per_million',
+        color='location',
+        tooltip = ['hosp_patients_per_million', 'location']
+    )
+    text = base.mark_text(align='left', dx=5, dy=-5).encode(
+    text=alt.condition(nearest, 'hosp_patients_per_million', alt.value(' '))
+    )
+elif selected_stat == 'ICU Admissions':
+    base = alt.Chart(df_g2
+    ).mark_line().encode(
+        x='date',
+        y='weekly_icu_admissions_per_million',
+        color='location',
+        tooltip = ['weekly_icu_admissions_per_million', 'location']
+    )
+    text = base.mark_text(align='left', dx=5, dy=-5).encode(
+    text=alt.condition(nearest, 'weekly_icu_admissions_per_million', alt.value(' '))
+    )
+else:
+    base = alt.Chart(df_g2
+    ).mark_line().encode(
+        x='date',
+        y='new_deaths_smoothed_per_million',
+        color='location',
+        tooltip = ['new_deaths_smoothed_per_million', 'location']
+    )
+    text = base.mark_text(align='left', dx=5, dy=-5).encode(
+    text=alt.condition(nearest, 'new_deaths_smoothed_per_million', alt.value(' '))
+    )
 
 
-### Slider for selection of data range ###
+points = base.mark_point().encode(
+    opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+)
+
+rules = alt.Chart(df_g2).mark_rule(color='gray').encode(
+    x='date',
+).transform_filter(
+    nearest
+)
+
+# wrap them together
+g2 = alt.layer(
+    base, selectors, points, rules, text
+).properties(
+    width=800, height=600
+)
+g2
+############### G2 ###############
+
+
+############### G3 ###############
+### Graph 3: Bar chart across different countries ###
+### Title of G3 ###
+st.write("#### National Conditions and Covid Statistics across Different Countries")
+#############
+
+### Slider for selection of date for culmulative data ###
 selected_date = st.slider("Select a date for culmulative statistics since 2020-01-01", min_value=min(df['date']), max_value=max(
     df['date']), value=datetime.date(2021, 1, 1), format="YYYY-MMM-DD")
 selected_date = str(selected_date)
@@ -126,25 +226,23 @@ conditions = st.multiselect(
 #     'Eritrea',
 #     'Guinea-Bissau'
 # ]
-default_countries = [
-    'Japan',
-    'United Kingdom',
-    'Italy',
-    'France',
-]
-option_countries = df["location"].unique()
-countries = st.multiselect(
-    "Select Countries", options=option_countries, default=default_countries)
 
-# should be removed later:
-# df_g3 = df[df["date"] == '2022-04-26']
+# default_countries = [
+#     'Japan',
+#     'United Kingdom',
+#     'Italy',
+#     'France',
+# ]
+# option_countries = df["location"].unique()
+# countries = st.multiselect(
+#     "Select Countries", options=option_countries, default=default_countries)
+
 
 df_g3 = df_g3[df_g3["location"].isin(countries)]
 df_g3_norm = df_g3_norm[df_g3_norm["location"].isin(countries)]
 #############
 
 
-### Graph 3: Bar chart across different countries ###
 # Header for G3
 st.write("#### National Conditions and Culmulative {} per Million People across Countries from 2020-01-01 to {}".format(selected_stat, selected_date))
 
@@ -176,8 +274,10 @@ bar_chart = alt.Chart(df_g3_long).mark_bar().encode(
         'conditions:N', title="National Conditions & {}".format(selected_stat)),
     row=alt.Row('location:N', title="Country", sort=countries),
     tooltip=["values:Q"]
-).properties(
-    width=440,
 )
+# .properties(
+#     width=800
+# )
 
 st.altair_chart(bar_chart, use_container_width=True)
+############### G3 ###############
